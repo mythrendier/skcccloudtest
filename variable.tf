56540be0aaf5b9886329415a5e688c208eb6f043
mythrendier <50357918+mythrendier@users.noreply.github.com>
2019-05-08 11:36:42 +0900

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
 variable "amazon_linux" {
 
   # Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type - ami-00a5245b4816c38e6 
- default = " ami-00a5245b4816c38e6 "
+ default = "ami-00a5245b4816c38e6"
 }
 
 variable "dev_keyname" {

3d15dc255adfebd3dbe21684b22382390f195413
EC2 Default User <ec2-user@ip-2-0-1-32.ap-northeast-1.compute.internal>
2019-05-08 02:16:40 +0000

    first commit

--- variable.tf
+++ variable.tf
@@ -1,9 +1,10 @@
 variable "amazon_linux" {
 
-  # Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type - ami-00a5245b4816c38e6  default = " ami-00a5245b4816c38e6 "
+  # Amazon Linux AMI 2018.03.0 (HVM), SSD Volume Type - ami-00a5245b4816c38e6 
+ default = " ami-00a5245b4816c38e6 "
 }
 
 variable "dev_keyname" {
-  default = " user02-keypair "
+  default = "user02-keypair"
 }
 
